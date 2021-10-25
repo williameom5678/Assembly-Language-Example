fb9f546bc2851c7d9685d1185c97bb0c7d5d7545
williameom <66811147+williameom5678@users.noreply.github.com>
2021-10-25 09:42:38 +0900

    Create 99beer.asm
    99 bottles of beer

---
+++ 99beer.asm
@@ -0,0 +1,39 @@
+;made by williameom
+;
+;    <HOW TO ASSEMBLE & LINK>
+; #> nasm -f elf game.asm
+; #> ld -I/lib/ld-linux.so.2 -lc game.o -o game
+;
+extern scanf
+extern printf
+
+segment .data
+  asdf db '%d', 0
+  left_makju db 'Beer Left => %d Bottles', 10, 0
+  no_makju db 'NO BEER LEFT', 10, 0
+
+section .bss
+
+segment .text
+global start
+start:
+  mov ebx, 100
+  mov ecx, ebx
+makju:
+  sub ebx, 1
+  push ebx
+  push left_makju
+  call printf
+
+  cmp ebx, 0
+  je end
+  loop makju
+
+end:
+  mov ecx, 0
+  push no_makju
+  call printf
+
+  mov eax, 1
+  mov ebx, 0
+  int 0x80
